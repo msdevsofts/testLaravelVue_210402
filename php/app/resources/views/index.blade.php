2b86d384c59cd81d483dbdb019c029808a721a93
msdevsofts <m.s.dev.softs@gmail.com>
2021-04-09 14:31:48 +0900

    show example

--- php/app/resources/views/index.blade.php
+++ php/app/resources/views/index.blade.php
@@ -5,9 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}" type="text/css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>Laravel + Vue.js</title>
 </head>
 <body>
 <h1>Laravel + Vue.js</h1>
+
+<div id="app">
+    <example-component></example-component>
+</div>
+<script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
